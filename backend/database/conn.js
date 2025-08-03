fe2d3ee2809a99679392eb89044c84d08275f7d1
kerlenmelo <kerlen_1@hotmail.com>
2025-08-03 14:01:52 -0300

    fix: alterado modo de conexão com o MongoDB.

--- backend/database/conn.js
+++ backend/database/conn.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      'mongodb+srv://kerlen:admin123@clusterjs.zgchwjs.mongodb.net/Projeto-WEB2?retryWrites=true&w=majority&appName=ClusterJS',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('Conectado ao MongoDB');
   } catch (error) {
     console.error('Erro ao conectar no MongoDB:', error);
